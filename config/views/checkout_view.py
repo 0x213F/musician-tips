f852cc328240536d1e9d6c15563c33b102db83ab
Josh Schultheiss <josh@insurgrid.com>
2020-11-19 00:12:46 -0800

    add missing import in checkout endpoint

--- config/views/checkout_view.py
+++ config/views/checkout_view.py
@@ -1,4 +1,6 @@
 import stripe
+import decimal
+from decimal import Decimal
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
--- config/views/receipt_view.py
+++ config/views/receipt_view.py
@@ -1,6 +1,3 @@
-import decimal
-from decimal import Decimal
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
